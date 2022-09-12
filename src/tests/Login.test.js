be879e2be53cc727b4a85de0280930cb46f7f92f
João Gomes <panterudo17100@gmail.com>
2022-09-12 11:26:10 -0300

    fix :req4

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -9,12 +9,17 @@ describe('Testando componente Login', () => {
     const testEmail = 'Alessandro@test.com'
     it('1-Verifica se o history redireciona para a página correta', () => {
         renderWithRouterAndRedux(<App />);
+
+
         const { history } = renderWithRouterAndRedux(<App />);
+
+
         expect(history.location.pathname).toBe('/');
       });
 
     it('2- Os inputs "email" e "nome" São renderizados', () => {
         renderWithRouterAndRedux(<App />);
+
 
         const inputName = screen.getByPlaceholderText(/nome/i);
         const inputEmail =  screen.getByPlaceholderText(/email/i);
@@ -25,16 +30,20 @@ describe('Testando componente Login', () => {
     it('3- Ao renderizar o componente "login", o botão está desabilitado', () => {
         renderWithRouterAndRedux(<App />);
 
+
         const playBtn = screen.getByTestId('btn-play');
+
 
         expect(playBtn).toBeDisabled();
     });
     it('4- Ao digitar nos inputs corretamente, o botão "play" será habilitado', () => {
         renderWithRouterAndRedux(<App />)
 
+
         const inputName = screen.getByPlaceholderText(/nome/i);
         const inputEmail =  screen.getByPlaceholderText(/email/i);
         const playBtn = screen.getByTestId('btn-play');
+
 
         userEvent.type(inputEmail, '');
         userEvent.type(inputName, '');
@@ -46,21 +55,33 @@ describe('Testando componente Login', () => {
     });
     it('5- Ao clicar no botão "play", o usuário será redirecionado para "/game"', async() => {
         const { history } = renderWithRouterAndRedux(<App />);
+
     
         const inputName = screen.getByPlaceholderText(/nome/i);
         const inputEmail =  screen.getByPlaceholderText(/email/i);
         const playBtn = screen.getByRole('button', { name: /play/i });
         
+
         userEvent.type(inputEmail, testEmail);
         userEvent.type(inputName, testName);
         userEvent.click(playBtn);
 
         // expect(history.location.pathname).toBe('/games');
     });
-    it('6- Ao clicar no botão "settings", o usuário será redirecionado para "/config"', () => {
-        const { history } = renderWithRouterAndRedux(<App />);
+    it('6-O botão de configurações é renderizado na tela', () => {
+        renderWithRouterAndRedux(<App />);
+
 
         const settingBtn = screen.getByTestId('btn-settings');
+
+        expect(settingBtn).toBeDefined();
+    });
+    it('7- Ao clicar no botão "settings", o usuário será redirecionado para "/config"', () => {
+        const { history } = renderWithRouterAndRedux(<App />);
+
+
+        const settingBtn = screen.getByTestId('btn-settings');
+
 
         userEvent.click(settingBtn);
 
